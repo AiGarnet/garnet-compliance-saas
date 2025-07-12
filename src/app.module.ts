2dc2bceb8a7cc23ce905bdbbde1b3e9de055f348
PR-ODINSON <prithraj120@gmail.com>
2025-07-12 13:56:15 +0530

    feedback section

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,7 @@ import { GenerateAnswersModule } from './generate-answers/generate-answers.modul
 import { ChecklistsModule } from './checklists/checklists.module';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { HelpModule } from './help/help.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 // Global providers
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
@@ -70,6 +71,7 @@ import { AppController } from './app.controller';
     ChecklistsModule,
     OrganizationsModule,
     HelpModule,
+    DashboardModule,
   ],
   providers: [
     // Global exception filter
